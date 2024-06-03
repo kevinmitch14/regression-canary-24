7b9a312cc662008291dfb06b5c9f3730585e14b2
kevinmitch14 <55989505+kevinmitch14@users.noreply.github.com>
2024-06-03 10:35:36 -0400

    cleanup

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,12 +1,8 @@
 import React from "react";
 import { testActionTestPage } from "../actions";
 
-export function delay(ms: number): Promise<void> {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 export default async function HomePage() {
-  await delay(200);
+  await new Promise((resolve) => setTimeout(resolve, 200));
   return (
     <div>
       <p>Time: {new Date().getTime()}</p>
